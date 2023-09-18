77184470b0aac89d251e7f68b5c1906eb3848f54
Tim Delaney <delanetw@mail.uc.edu>
2023-09-17 21:44:32 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double myLunchCost = 12.50;
+        System.out.println("The cost of my lunch is: " + myLunchCost);
+    }
+}
